c9abd07d0cf95be67ce65c7012ec719146eac616
MaksChyrkov <46497173+MaksChyrkov@users.noreply.github.com>
2020-10-01 21:32:33 +0300

    Used OOP Classes to refactor code on js. Added img support

--- src/model.js
+++ src/model.js
@@ -1,20 +1,20 @@
+import image from "./assets/websitebuilder.png"
+import {ImageBlock, TextBlock, TextColumnsBlock, TitleBlock} from "./classes/blocks";
+
 export const model = [
-    {
-        type: "title",
-        value: "Test Title",
-        options: {
-            styles: "background: darkred; color: #fff; text-align: center",
-            tag: "h2"
-        }
-    },
-    {
-        type: "text",
-        value: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquid atque deleniti",
-        options: {styles: "background: darkblue; color: yellow"}
-    },
-    {
-        type: "textColumns",
-        value: ['1 text', '2 text', '3 text'],
-        options: {styles: "padding: 1rem"}
-    }
+    new TitleBlock("Test Title", {
+        styles: "background: darkred; color: #fff; text-align: center",
+        tag: "h2"
+    }),
+    new ImageBlock(image, {
+        styles: "padding: 2rem 0; display: flex; justify-content: center",
+        alt: "my image",
+        imageStyles: "width: 500px; height: auto"
+    }),
+    new TextBlock("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquid atque deleniti", {
+        styles: "background: darkblue; color: yellow"
+    }),
+    new TextColumnsBlock(['1st column', '2nd column', '3rd column'], {
+        styles: "padding: 1rem"
+    })
 ]
