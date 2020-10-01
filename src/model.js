4eb613fde78d5346db21d63eeca68a5234ef3868
MaksChyrkov <46497173+MaksChyrkov@users.noreply.github.com>
2020-10-01 18:01:26 +0300

    Added a model in model.js to create html elements easily

---
+++ src/model.js
@@ -0,0 +1,9 @@
+export const model = [
+    {type: "title", value: "Test Title", options: {}},
+    {
+        type: "text",
+        value: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquid atque deleniti",
+        options: {}
+    },
+    {type: "textColumns", value: ['1 text', '2 text', '3 text'], options: {}}
+]
