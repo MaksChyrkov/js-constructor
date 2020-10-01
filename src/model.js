bab104ec34a803e8e2310fc7800a56601869db4e
MaksChyrkov <46497173+MaksChyrkov@users.noreply.github.com>
2020-10-01 19:29:10 +0300

    Refactored javascript code, so it's now easier to develop an app

--- src/model.js
+++ src/model.js
@@ -1,9 +1,20 @@
 export const model = [
-    {type: "title", value: "Test Title", options: {}},
+    {
+        type: "title",
+        value: "Test Title",
+        options: {
+            styles: "background: darkred; color: #fff; text-align: center",
+            tag: "h2"
+        }
+    },
     {
         type: "text",
         value: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquid atque deleniti",
-        options: {}
+        options: {styles: "background: darkblue; color: yellow"}
     },
-    {type: "textColumns", value: ['1 text', '2 text', '3 text'], options: {}}
+    {
+        type: "textColumns",
+        value: ['1 text', '2 text', '3 text'],
+        options: {styles: "padding: 1rem"}
+    }
 ]
